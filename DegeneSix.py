ac63870d91cd9a1d296c6e8494e1e689fa3a83f6
David <daviegourevitch@gmail.com>
2020-06-15 21:10:54 -0400

    Done

--- DegeneSix.py
+++ DegeneSix.py
@@ -1,17 +1,16 @@
 # Work with Python 3.6
+import numpy as np
 import os
 import discord
-from discord.ext.commands import Bot, when_mentioned_or
 import sqlite3
 import datetime
 import math
-import numpy as np
+from discord.ext.commands import Bot, when_mentioned_or
 
-# Setup Bot
-BOT_PREFIX = ("!")
-TOKEN = os.environ.get('TOKEN')
+BOT_PREFIX = ("?", "!")
+TOKEN = os.environ.get('ACCESS_TOKEN') # Get at discordapp.com/developers/applications/me
+
 bot = Bot(command_prefix=when_mentioned_or(*BOT_PREFIX))
-print("Current token: " + TOKEN)
 
 # Setup database
 try:
@@ -23,6 +22,7 @@ except Exception as e:
 	print(e)
 	print("Database error")
 	exit()
+
 
 @bot.command(
     name='Degene6',
@@ -40,7 +40,7 @@ async def degenesix(context,actionNumber:int,difficulty=0):
     ones = (roll == 1).sum()
 
     if difficulty:
-        result = ('*Success!* <:degenesis:721181871345631253>\n' if successes >= difficulty else "Failure!\n") if ones <= successes else '*It\'s a botch!* :skull:\n'
+        result = ('*Success!* <:degenesis:684046972134490130>\n' if successes >= difficulty else "Failure!\n") if ones <= successes else '*It\'s a botch!* :skull:\n'
         msg = "%s needs %d successes and rolls:" % (context.author.mention,difficulty) if autos == 0 else "%s needs %d successes, already has %d automatic and rolls:" % (context.author.mention,difficulty,autos)
     else:
         result = '' if ones <= successes else '*It\'s a botch!* :skull:\n'
@@ -49,6 +49,43 @@ async def degenesix(context,actionNumber:int,difficulty=0):
     successes,
     triggers,
     result)
+    await context.send(msg)
+
+@bot.command(
+    name='GegromeGetroll',
+    description="Rolls a hacked Degenesis dice pool.",
+    brief="I am the creator.",
+    aliases=['GeeGee6','GeromeGetroll'],
+    pass_context=True)
+async def degenesix(context,actionNumber:int,difficulty=0):
+    autos = 0 if actionNumber < 13 else actionNumber-12
+    actionNumber = 12 if actionNumber > 13 else actionNumber
+    roll = np.random.choice([1,5,6],actionNumber,p=[0.25,0.25,0.5])
+    successes = (roll > 3).sum()
+    successes += autos
+    triggers = (roll == 6).sum()
+    ones = (roll == 1).sum()
+
+    if difficulty:
+        result = ('*Just as planned* <:rg:684046905180684288>\n' if successes >= difficulty else "Failure!\n") if ones <= successes else '*Marauders fucking with my shit again* <:Getrellno:550654128238624768>\n'
+        msg = "GG needs %d successes and rolls:" % (difficulty) if autos == 0 else "GG needs %d successes, already had %d cryofreezed and rolls:" % (difficulty,autos)
+    else:
+        result = '' if ones <= successes else '*Marauders fucking with my shit again* <:Getrellno:550654128238624768>\n'
+        msg = "GG had always planned to roll:" if autos == 0 else "GG acquired %d automatic successes back in 2079 and rolls:" % (autos)
+    msg+= " \n %s \n %d successes, %d triggers \n %s" % (', '.join(map(str,roll)),
+    successes,
+    triggers,
+    result)
+    await context.send(msg)
+
+@bot.command(
+    name='DevInfo',
+    description="Developer Information",
+    brief="Code availability",
+    aliases=['DI'],
+    pass_context=False)
+async def degenesix(context):
+    msg = "Code available at: https://github.com/MrMstislav/DiscordDegeneSix/\n"
     await context.send(msg)
 
 ########################## Roll utilities ##########################
@@ -124,8 +161,7 @@ async def initiativeStart(context, label:str=None):
 		await context.send(msg)
 		await cleanupDB()
 	except Exception as e:
-		await context.send("Failed to start initiative. Try a different channel, or email daviegourevitch@gmail.com for immediate help")
-		await context.send(e)
+		await context.send("Failed to start initiative. Try a different channel, or ping <@154353119352848386> for immediate help")
 
 async def cleanupDB():
 	global cursor, connection
@@ -141,11 +177,9 @@ async def cleanupDB():
 				cursor.execute("DELETE FROM initiatives WHERE channel_id=?", (id,))
 				cursor.execute("DELETE FROM characters WHERE channel_id=?", (id,))
 				cursor.execute("DELETE FROM initiative_values WHERE channel_id=?", (id,))
-				print("deletted")
 			connection.commit()
 	except Exception as e:
-		print(e)
-
+	    print("Error while cleaning database")
 
 ########################## Add to Initiative ##########################
 @bot.command(
@@ -189,7 +223,7 @@ async def initiativeAdd(context, *args):
 		msg += "Player " + (name if name else context.author.mention) + " was added to the initiative with " + str(dice) + " dice" + ((" and " + str(ego) + " ego") if ego else "")
 		await context.send(msg)
 	except Exception as e:
-		await context.send("An error occurred while adding you to the initiative")
+		await context.send("An error occurred while adding you to the initiative. Ping <@154353119352848386> for immediate help")
 
 def parseInitiativeAdd(args):
 	try:
@@ -271,7 +305,7 @@ async def initiativeNext(context, *args):
 		# Do the turn
 		cursor.execute("SELECT mention, name, num_ego, num_successes, num_triggers FROM characters WHERE channel_id=? AND num_successes=?", (context.channel.id, cur_initiative))
 		characters = cursor.fetchall()
-		msg += str(cur_initiative) + " successe(s):\n"
+		msg += str(cur_initiative) + " successes\n"
 		for character in characters:
 			msg += character[0] + ", it is " + (character[1] + "\'s turn. " if character[1] else "your turn.")
 			extraActions = math.floor(character[4]/2)
@@ -295,8 +329,7 @@ async def initiativeNext(context, *args):
 		connection.commit()
 
 	except Exception as e:
-		await context.send("An error occurred while moving to next initiative")
-		await context.send(e)
+		await context.send("An error occurred while moving to next initiative. Ping <@154353119352848386> for immediate help")
 
 
 def sortCharactersBySuccesses(characters):
@@ -323,10 +356,8 @@ async def debugDB(context, field:str):
 	except Exception as e:
 		await context.send(e)
 
-
 @bot.event
 async def on_ready():
     print("Logged in as " + bot.user.name)
-
 
 bot.run(TOKEN)
